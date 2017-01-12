911b98af04b57a1d0094c0ef83a31cc551465a10
Teng-Hui Huang <hui811116@outlook.com>
2017-01-12 22:22:17 +0800

    interference detector added

---
+++ include/lsa/interference_energy_detector_cc.h
@@ -0,0 +1,61 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2017 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+
+#ifndef INCLUDED_LSA_INTERFERENCE_ENERGY_DETECTOR_CC_H
+#define INCLUDED_LSA_INTERFERENCE_ENERGY_DETECTOR_CC_H
+
+#include <lsa/api.h>
+#include <gnuradio/block.h>
+
+namespace gr {
+  namespace lsa {
+
+    /*!
+     * \brief <+description of block+>
+     * \ingroup lsa
+     *
+     */
+    class LSA_API interference_energy_detector_cc : virtual public gr::block
+    {
+     public:
+      typedef boost::shared_ptr<interference_energy_detector_cc> sptr;
+
+      /*!
+       * \brief Return a shared_ptr to a new instance of lsa::interference_energy_detector_cc.
+       *
+       * To avoid accidental use of raw pointers, lsa::interference_energy_detector_cc's
+       * constructor is in a private implementation
+       * class. lsa::interference_energy_detector_cc::make is the public interface for
+       * creating new instances.
+       */
+      static sptr make(
+        const std::string& ed_tagname,
+        const std::string& voe_tagname,
+        float ed_threshold,
+        float voe_threshold,
+        size_t blocklength);
+    };
+
+  } // namespace lsa
+} // namespace gr
+
+#endif /* INCLUDED_LSA_INTERFERENCE_ENERGY_DETECTOR_CC_H */
+
---
+++ lib/interference_energy_detector_cc_impl.h
@@ -0,0 +1,74 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2017 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_LSA_INTERFERENCE_ENERGY_DETECTOR_CC_IMPL_H
+#define INCLUDED_LSA_INTERFERENCE_ENERGY_DETECTOR_CC_IMPL_H
+
+#include <lsa/interference_energy_detector_cc.h>
+
+namespace gr {
+  namespace lsa {
+
+    class interference_energy_detector_cc_impl : public interference_energy_detector_cc
+    {
+     private:
+      // Nothing to declare in this block.
+      float* d_energy_reg;
+      float* d_voe_reg;
+      int d_blocklength;
+      //size_t d_buffer_length;
+      float d_ed_thres_db;
+      float d_voe_thres_db;
+
+      pmt::pmt_t d_voe_tagname;
+      pmt::pmt_t d_ed_tagname;
+
+     public:
+      interference_energy_detector_cc_impl(
+        const std::string& ed_tagname,
+        const std::string& voe_tagname,
+        float ed_threshold,
+        float voe_threshold,
+        size_t blocklength);
+      ~interference_energy_detector_cc_impl();
+
+      // Where all the action really happens
+      void forecast (int noutput_items, gr_vector_int &ninput_items_required);
+
+      int general_work(int noutput_items,
+           gr_vector_int &ninput_items,
+           gr_vector_const_void_star &input_items,
+           gr_vector_void_star &output_items);
+
+      void set_ed_threshold(float threshold_db);
+      float ed_threshold() const;
+
+      void set_voe_threshold(float threshold_db);
+      float voe_threshold() const;
+
+      void set_blocklength(size_t blocklength);
+      size_t blocklength() const;
+    };
+
+  } // namespace lsa
+} // namespace gr
+
+#endif /* INCLUDED_LSA_INTERFERENCE_ENERGY_DETECTOR_CC_IMPL_H */
+
